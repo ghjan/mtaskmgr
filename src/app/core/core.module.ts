e501420cf3b59365202019eb9f259a2b31b471e3
jian.zhang <cajan2@163.com>
2018-09-11 18:06:08 +0800

    new project;

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent, FooterComponent, SidebarComponent
+  ],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf()parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已经存在，不能再次加载');
+    }
+  }
+
+}
